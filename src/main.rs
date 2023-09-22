ba98c3974d97c86c6c4652b32fcdff7bceff216f
Nertsal <sasha.kudasov04@gmail.com>
2023-09-22 15:48:47 +0300

    Fix geng version

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,14 @@ fn main() {
 
     let opts: Opts = clap::Parser::parse();
 
-    let geng = Geng::new_with(geng::ContextOptions {
-        title: "Geng Game".to_string(),
-        ..geng::ContextOptions::from_args(&opts.geng)
-    });
+    let mut geng_opts = geng::ContextOptions::default();
+    geng_opts.window.title = "Doodle Shoot".to_string();
+    geng_opts.with_cli(&opts.geng);
 
-    let future = {
-        let geng = geng.clone();
-        async move {
-            let manager = geng.asset_manager();
-            let assets = assets::Assets::load(manager).await.unwrap();
-            game::Game::new(&geng, &Rc::new(assets))
-        }
-    };
-    geng.run_loading(future)
+    Geng::run_with(&geng_opts, |geng| async move {
+        let manager = geng.asset_manager();
+        let assets = assets::Assets::load(manager).await.unwrap();
+        let game = game::Game::new(&geng, &Rc::new(assets));
+        geng.run_state(game).await;
+    });
 }
