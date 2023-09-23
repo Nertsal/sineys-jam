195af898286a66616206d42eef9e6ec0332bcc93
kuviman <kuviman@gmail.com>
2023-09-23 20:46:45 +0400

    end?

--- src/end_screen.rs
+++ src/end_screen.rs
@@ -4,14 +4,16 @@ pub struct EndScreen {
     geng: Geng,
     assets: Rc<assets::Assets>,
     transition: Option<geng::state::Transition>,
+    score: i32,
 }
 
 impl EndScreen {
-    pub fn new(geng: &Geng, assets: &Rc<assets::Assets>) -> Self {
+    pub fn new(geng: &Geng, assets: &Rc<assets::Assets>, score: i32) -> Self {
         Self {
             geng: geng.clone(),
             assets: assets.clone(),
             transition: None,
+            score,
         }
     }
 }
@@ -19,6 +21,18 @@ impl EndScreen {
 impl geng::State for EndScreen {
     fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
         ugli::clear(framebuffer, Some(Rgba::BLACK), None, None);
+        self.geng.default_font().draw(
+            framebuffer,
+            &geng::Camera2d {
+                center: vec2::ZERO,
+                rotation: Angle::ZERO,
+                fov: 10.0,
+            },
+            &format!("GG\n\nyour score:\n{}", self.score),
+            vec2::splat(geng::TextAlign::CENTER),
+            mat3::translate(vec2(0.0, 3.0)),
+            Rgba::WHITE,
+        )
     }
     fn handle_event(&mut self, event: geng::Event) {
         if let geng::Event::KeyPress { key: geng::Key::R } = event {
